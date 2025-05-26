bc609c583acd6fa17cb257f9d31a63a525a8cd36
AIR01 <erwann.bruneaupro@gmail.com>
2025-05-26 12:21:15 +0200

    Netoyage de code

--- src/main/java/com/openclassrooms/tourguide/service/RewardsService.java
+++ src/main/java/com/openclassrooms/tourguide/service/RewardsService.java
@@ -2,7 +2,6 @@ package com.openclassrooms.tourguide.service;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import org.springframework.stereotype.Service;
